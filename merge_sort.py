e15a7def11dcc240ca8e8059efbaef4c3e9ed2e8
U-INFORMATICA\vshriniv <vshriniv@INW00004659.informatica.com>
2015-08-06 01:28:03 +0530

    alternative merge option

--- merge_sort.py
+++ merge_sort.py
@@ -27,9 +27,7 @@ def merge(L1, L2, compareFn):
 	return mergedList
 
 
-
 import operator
-
 def mergeSort(L):
 	'''
 	This is the function which basically breaks
@@ -43,3 +41,37 @@ def mergeSort(L):
 		left = mergeSort(L[:middle])
 		right = mergeSort(L[middle:])
 		return merge(left, right, operator.lt)
+
+
+
+
+
+		
+# ===============================================================
+def merge2(L1, L2, compareFn):
+	'''
+	The only difference between this and above is
+	the appending of remaining elements into the 
+	sorted list.
+	'''
+	i, j = 0, 0
+	mergedList = []
+
+	while i < len(L1) and j < len(L2):
+		if compareFn(L1[i], L2[j]):
+			mergedList.append(L1[i])
+			i += 1
+		else:
+			mergedList.append(L2[j])
+			j += 1
+
+	# you cannot simply append the lists at the end
+	#		since, you need to compare every element
+	#		Here, unlike above, we are using python's 
+	#		list comprehensions
+	if i < len(L1):
+		mergedList.append(L1[i:])
+	if j < len(L2):
+		mergedList.append(L2[j:])
+
+	return mergedList
