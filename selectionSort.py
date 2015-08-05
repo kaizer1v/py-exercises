8120e0c86203f3a9f31ab3a9249a01779518c0d0
U-INFORMATICA\vshriniv <vshriniv@INW00004659.informatica.com>
2015-08-05 17:13:19 +0530

    explained selection sort

--- selectionSort.py
+++ selectionSort.py
@@ -16,3 +16,29 @@ def selectionSort(L):
 		prefix += 1
 
 	return L
+
+
+
+
+
+def selectionSort2(L):
+
+	# assume a sorted part of the list's index
+	lastSortedIndex = 0
+
+	# from the last sorted index, until the end of the list
+	while lastSortedIndex < len(L):
+
+		# loop through every element
+		for currIndex in range(lastSortedIndex + 1, len(L)):
+
+			# and if you find any element which is less than the last sorted element.
+			if L[currIndex] < L[lastSortedIndex]:
+				# then swap it
+				L[currIndex], L[lastSortedIndex] = L[lastSortedIndex], L[currIndex]
+
+		# Now, increment the last sorted index by 1
+		lastSortedIndex += 1
+
+	# Finally, return the sorted list
+	return L
