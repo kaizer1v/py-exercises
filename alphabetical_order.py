8519ee3d978b2a551e9f1c5acc5070479c54cdf3
kaizer1v <kaizer1v@gmail.com>
2017-11-15 13:24:37 +0530

    fixed flake8 errors

--- alphabetical_order.py
+++ alphabetical_order.py
@@ -1,23 +1,26 @@
-def longestSubstr():
-	"""
-	Given a long string, we need to find the longest substring where all the letters in
-	increasing order
-	"""
+def longest_substr(s):
+    """
+    Given a long string, we need to find the longest
+    substring where all the letters in increasing order
+    """
 
-	s = 'feeagrepoasdgrepasdjrepgassbrebgaspdodge'
-	alphabets = 'abcdefghijklmnopqrstuvwxyz'
-	substr = s[0]
-	longest = ''
-	for i in range(1, len(s)):
-		if alphabets.index(substr[-1:]) <= alphabets.index(s[i]):
-	    substr += s[i]
-		else:
-	    if len(substr) > len(longest):
-	      longest = substr
-	    substr = s[i]
+    alphabets = 'abcdefghijklmnopqrstuvwxyz'
+    substr = s[0]
+    longest = ''
+    for i in range(1, len(s)):
+        if alphabets.index(substr[-1:]) <= alphabets.index(s[i]):
+            substr += s[i]
+        else:
+            if len(substr) > len(longest):
+                longest = substr
+            substr = s[i]
 
-	# to check for the last series of characters
-	if len(substr) > len(longest):
-	  longest = substr
-	    
-	return longest
+    # to check for the last series of characters
+    if len(substr) > len(longest):
+        longest = substr
+
+    return longest
+
+
+print longest_substr('feeagrepoasdgrepasdjrepgassbrebgaspdodge')
+print longest_substr('feeaabcdeilnxygrepasdwhatthefuckaspdodge')
