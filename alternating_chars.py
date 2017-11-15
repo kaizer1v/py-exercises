3ed8408e156832ed11166601a3c76779aade9c81
kaizer1v <kaizer1v@gmail.com>
2017-11-15 16:42:51 +0530

    fixing flake8 errors

--- alternating_chars.py
+++ alternating_chars.py
@@ -1,13 +1,19 @@
-# Given a string like AABABBAB, you need to return how many chars need to be
-# deleted so that no subsequent letters are the same
-# Basically, AABABABBA should become ABABABA (return total chars to be deleted)
+def alternating_chars(s):
+    # Given a string like AABABBAB, you need to return how many chars need to be
+    # deleted so that no subsequent letters are the same
+    # Basically, AABABABBA should become ABABABA (return total chars to be
+    # deleted)
 
-total = int(input())
-while(total):
-    str = input()
+    # total = len(s)
+    # while(total):
     toDel = 0
-    for x in range(1, len(str)):
-        if str[x] == str[x - 1]:
+    for x in range(1, len(s)):
+        if s[x] == s[x - 1]:
             toDel = toDel + 1
-    print(toDel)
-    total = total - 1
+    # total = total - 1
+
+    for index, char in enumerate(s):
+        if s[index] == s[index - 1]
+
+
+alternating_chars('AABABABBA')
