0ba47f67e3a74c4076d9b535fbc34c304449ad97
kaizer1v <kaizer1v@gmail.com>
2017-11-15 16:54:22 +0530

    fixing flake8 errors

--- count_bob.py
+++ count_bob.py
@@ -1,13 +1,20 @@
-"""
-Write a program that prints the number of times the string 'bob' occurs in s. For example, if s = 'azcbobobegghakl', 
-then your program should print the number of times the string 'bob' appears in the string.
-"""
+def count_match(s, match):
+    '''
+    Write a program that prints the number of
+    times the string 'bob' occurs in s.
+    For example, if s = 'azcbobobegghakl',
+    then your program should print the number
+    of times the string 'bob' appears in the string.
+    '''
+    count = 0
+    while 'b' in s:
+        bi = s.index('b')
+        if s[bi:bi + 3] == match:
+            count += 1
+        s = s[bi + 1:]
+    print count
 
-count = 0
-while 'b' in s:
-  bi = s.index('b')
-  if s[bi:bi+3] == 'bob':
-    count += 1
-  s = s[bi+1:]
-  #print bi, s, count
-print count
+
+count_match('bobaobboaodbaobobaobbob', 'bob')
+count_match('booboboodobdobobodobdob', 'bob')
+count_match('abdabadbababab', 'bab')
