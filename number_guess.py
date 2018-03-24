a43f3f8af4f341c25a14075570126f9b87785043
kaizer1v <kaizer1v@gmail.com>
2018-03-24 13:52:55 +0530

    only return integer values and converted to python3

--- number_guess.py
+++ number_guess.py
@@ -1,21 +1,22 @@
 # Paste your code into this box
 
-print 'Please think of a number between 0 and 100! '
+print('Please think of a number between 0 and 100! ')
 
 low = 0
 high = 100
 guess = (low + high) / 2
 lhc = ''
 while lhc != 'c':
-	lhc = raw_input('Is your number %d? ' % guess + '? \nEnter \'h\' to indicate the guess is too high. Enter \'l\' to indicate the guess is too low. Enter \'c\' to indicate I guessed correctly. ')
-	if lhc == 'l':
-		low = guess
-	elif lhc == 'h':
-		high = guess
-	elif lhc == 'c':
-		break
-	else:
-		print 'Sorry, I did not understand your input.'
-	guess = (low + high) / 2
+    lhc = input(
+        'Is your number %d? {} ? \nEnter \'h\' to indicate the guess is too high. Enter \'l\' to indicate the guess is too low. Enter \'c\' to indicate I guessed correctly.'.format(guess))
+    if lhc == 'l':
+        low = guess
+    elif lhc == 'h':
+        high = guess
+    elif lhc == 'c':
+        break
+    else:
+        print('Sorry, I did not understand your input.')
+    guess = (low + high) // 2
 
-print 'Game over. Your secret number was ', guess
+print('Game over. Your secret number was {}'.format(guess))
