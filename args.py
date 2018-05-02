7000493c0683eaf1e25acfa2a510b9715c976654
kaizer1v <kaizer1v@gmail.com>
2018-05-02 23:28:29 +0530

    dict doesn't have any method called `iteritems`

--- args.py
+++ args.py
@@ -10,7 +10,7 @@ first(123, 'asdf', False, 1.55, [1, 2, 4], {'a': 23}, (33, 55, 77))
 def second(first, **kwargs):
     '''testing out kwargs i.e. keyword arguments'''
     # print first
-    for key, value in kwargs.iteritems():
-        print("{} ==> {}".format(key, value))
+    for key in kwargs:
+        print("{} ==> {}".format(key, kwargs[key]))
 
 second('second', str="vivek", float=12.34, bool=True, ll=[1, 5, 3])
