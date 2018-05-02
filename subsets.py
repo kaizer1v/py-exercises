60b8a8a2ea8ccbcece47b4f23c8c6f5b7c7391f2
kaizer1v <kaizer1v@gmail.com>
2018-05-02 23:22:54 +0530

    using sets in py36

--- subsets.py
+++ subsets.py
@@ -1,41 +1,47 @@
 def genSubsets(L):
-  '''
-  Given a list of integers. This function
-  finds all the subsets that can be formed with
-  these list elements. This returned object will
-  be a list of (lists of subsets).
-  '''
-  res = []
-  if len(L) == 0:
-    return [[]]
-  smaller = genSubsets(L[:-1])
-  extra = L[-1:]
-  new = []
-  for small in smaller:
-    new.append(small+extra)
-  
-  #print 'smaller: ', smaller, 'extra: ', extra, 'new: ', new
-  return smaller + new
-    
-# Example 1    
+    '''
+    Given a list of integers. This function
+    finds all the subsets that can be formed with
+    these list elements. This returned object will
+    be a list of (lists of subsets).
+    '''
+    res = []
+    if len(L) == 0:
+        return [[]]
+    smaller = genSubsets(L[:-1])
+    extra = L[-1:]
+    new = []
+    for small in smaller:
+        new.append(small + extra)
+
+    # print 'smaller: ', smaller, 'extra: ', extra, 'new: ', new
+    return smaller + new
+
+# Example 1
 genSubsets([1, 2, 3])
 
 
-
 def isSubset(L1, L2):
-  '''
-  Assumes L1 and L2 are lists.
-  Returns True if each element of L1 is also in L2
-  and False otherwise.
-  '''
-  if len(L1) == 0 and len(L2) == 0:
-    return False
-  for e1 in L1:
-    matched = False
-    for e2 in L2:
-      if e1 == e2:
-        matched = True
-        break
-    if not matched:
-      return False
-  return True
+    '''returns `True` if L1 is subset of L2, `False` otherwise'''
+    s1 = set(L1)
+    s2 = set(L2)
+    return s1.issubset(s2)
+
+
+def isSubsetOld(L1, L2):
+    '''
+    Assumes L1 and L2 are lists.
+    Returns True if each element of L1 is also in L2
+    and False otherwise.
+    '''
+    if len(L1) == 0 and len(L2) == 0:
+        return False
+    for e1 in L1:
+        matched = False
+        for e2 in L2:
+            if e1 == e2:
+                matched = True
+                break
+        if not matched:
+            return False
+    return True
