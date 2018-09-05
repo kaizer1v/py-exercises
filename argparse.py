089d516e2acb785db6ca4780907da23e13a4928c
kaizer1v <kaizer1v@gmail.com>
2018-09-05 12:19:59 +0530

    filname happened to be the same name as module name - thus a conflict

--- argparse.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(
-    description='This is a PyMOTW sample program'
-)
-
-parser.add_argument('-a', action="store_true", default=False)
-
-print(parser.parse_args(['-a', '-bval', '-c', '3']))
