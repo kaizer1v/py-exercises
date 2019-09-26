f212e8529cc4b24a996e32a2f5a09bd0d28cb2ce
kaizer1v <kaizer1v@gmail.com>
2019-09-26 10:04:07 +0530

    bug fix

--- algo-princeton/linked_list.py
+++ algo-princeton/linked_list.py
@@ -5,37 +5,114 @@ or a string or even objects
 '''
 class Node:
 
-  def get_data(self):
-    return self.data
-
-  def set_data(self, data):
-    self.data = data
-
   def get_next(self):
     return self.next_node
 
   def set_next(self, node):
     self.next_node = node
 
+  def get_data(self):
+    return self.data
+
+  def set_data(self, data):
+    self.data = data
+
   def __init__(self, data, next_node=None):
     self.data = data
     self.next_node = next_node
+
+
+class LinkedListExpectsNode:
+
+  def is_empty(self):
+    return self.head == None
+
+  def insert(self, node):
+    if not self.is_empty():
+      node.set_next(self.head)
+    self.head = node
+
+  def remove(self, data):
+    if self.is_empty():
+      return None
+
+    # if there's only one item
+    if self.head.get_data() == data:
+      self.head = self.head.get_next()
+      return True
+
+    node = self.head
+    while node.get_next() != None:
+      if node.get_next().get_data() == data:
+        node.set_next(node.get_next().get_next())
+        return True
+      node = node.get_next()
+
+    # last item
+    if node.get_next() == None:
+      if node.get_data() == data:
+        self.head = None
+        return True
+
+    return False
+
+  def find(self, data):
+    if self.is_empty():
+      return None
+
+    node = self.head
+    while node.get_next() != None:
+      if node.get_data() == data:
+        return True
+      node = node.get_next()
+
+    if node.get_next() == None:
+      return node.get_data() == data
+
+    return False
+
+  def print(self):
+    if self.is_empty():
+      return 'Empty'
+
+    node = self.head
+    p = []
+    while node.get_next() != None:
+      p.append(node.get_data())
+      node = node.get_next()
+    p.append(node.get_data())
+    print(' -> '.join(list(map(str, p))))
+
+  def __init__(self):
+    self.head = None
 
 '''
 Let's try to create a list of nodes like so
 6 -> 3 -> 4 -> 2 -> 1
 '''
 
-# node6 = Node(6)
-# node3 = Node(3)
-# node4 = Node(4)
-# node2 = Node(2)
-# node1 = Node(1)
+node6 = Node(6)
+node3 = Node(3)
+node4 = Node(4)
+node2 = Node(2)
+node1 = Node(1)
 
-# node6.set_next(node3)
-# node3.set_next(node4)
-# node4.set_next(node2)
-# node2.set_next(node1)
+ll = LinkedListExpectsNode()
+ll.insert(node6)
+ll.insert(node3)
+ll.insert(node4)
+ll.insert(node2)
+ll.insert(node1)
+
+# print('2', ll.find(2))
+# print('6', ll.find(6))
+# print('4', ll.find(4))
+# print('5', ll.find(5))
+
+ll.print()
+ll.remove(3)
+ll.print()
+
 
 
 '''
@@ -53,59 +130,6 @@ def countNodes(head):
     incr += 1
 
   return incr
-
-
-
-
-
-class LinkedList:
-
-  def insert(self, n_data):
-    node = Node(n_data, self.head)
-    self.head = node
-
-  def remove(self, n_data):
-    pass
-
-  def find(self, n_data):
-    node = self.head
-    while node.get_next() != None:
-      if node.get_data() == n_data:
-        return True
-      node = node.get_next()
-    return node.get_data() == n_data
-
-  def print(self):
-    node = self.head
-    while node.get_next() != None:
-      print(node.get_data())
-      node = node.get_next()
-    if node.get_next() == None:
-      print(node.get_data())
-
-  def is_empty(self):
-    return self.head == None
-
-  def get_head(self):
-    return self.head
-
-  def __init__(self):
-    self.head = None
-
-
-# ll = LinkedList()
-# ll.insert(6)
-# ll.insert(3)
-# ll.insert(4)
-# print(ll.print())
-# print(ll.find(4))
-# ll.remove(3)
-# ll.insert(2)
-# ll.remove(6)
-# ll.insert(1)
-# print(ll.find(2))
-
-
 
 
 '''
@@ -152,12 +176,12 @@ class LinkedListIterable:
     self.head = None
 
 
-lli = LinkedListIterable()
-lli.insert(6)
-lli.insert(4)
-lli.insert(3)
-lli.insert(1)
-lli.insert(2)
+# lli = LinkedListIterable()
+# lli.insert(6)
+# lli.insert(4)
+# lli.insert(3)
+# lli.insert(1)
+# lli.insert(2)
 
 # for elem in lli:
 #   print('>>>>>', elem)
